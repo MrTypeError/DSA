46c30ef4682a4116086670f0a54618eb401c601e
MrTypeError <sanugta8@gmail.com>
2022-05-29 00:56:46 +0530

    Completed Code For CircularLL

--- Link Lists/CircularLinkList.py
+++ Link Lists/CircularLinkList.py
@@ -1,3 +1,6 @@
+from ast import While
+
+
 class Node():
     def __init__(self,data) -> None:
         self.data=data
@@ -8,9 +11,14 @@ class CircularLinkList(self):
         self.head=None
     
     def PushInLL(self,data):
-        pass
-    def DelInLL(self):
-        pass
+        RealNode=Node(Data)
+        temp=self.head
+        if temp.next != None:
+            while(temp.next != self.head):
+                temp=temp.next
+            temp.next=RealNode
+        else:
+            RealNode.next=RealNode
     def ShowInLL(self):
         temp=self.head
         if temp is not None:
@@ -19,6 +27,15 @@ class CircularLinkList(self):
                 temp=temp.next
                 if temp==self.head:
                     break
-                
+ObjectForLL=CircularLinkList()
 
-        
+ObjectForLL.PushInLL(201)
+ObjectForLL.PushInLL(204)
+ObjectForLL.PushInLL(112)
+ObjectForLL.PushInLL(300)
+ObjectForLL.PushInLL(101)
+print("You Have Pushed Some Elements " )
+print("Those are given below : ")
+ShowingInfo=ObjectForLL.ShowInLL()
+print(ShowingInfo)
+
