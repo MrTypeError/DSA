f32029b05ebc8b142280f6d732a14f086c59c584
MrTypeError <sanugta8@gmail.com>
2022-06-09 11:15:07 +0530

    Change Done

--- Queue/Queue_with_stack_implimentation.py
+++ Queue/Queue_with_stack_implimentation.py
@@ -12,7 +12,7 @@ def Enqueue(self,data):
          item=self.arr2.pop()
          self.arr1.append(item)
 
-def Dqueue(self): #ghgh
+def Dqueue(self):
     if len(self.arr1) == 0:
         print("Under Flow")
     else:
