97afa784542870c2e49a32eeb5630ffb848511b0
MrTypeError <sanugta8@gmail.com>
2022-06-12 19:08:33 +0530

    Code Is In Progress

--- Recurssion/RecurssionEmoji.py
+++ Recurssion/RecurssionEmoji.py
@@ -1,13 +1,15 @@
 def design(num, spc=0):
 
     if num == 0:
+        print("Another One")
         return
+        
 
     if(num%2==0):
         spc += 2
         print(((" :) "*num) + " " * (spc*2-4)*2 + ":" + (str(num))))
         design(num-2, spc)
-print("Another One")
+
 
 
 
