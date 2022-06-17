cdded63feb64cd2d3bea4421aa32c57348ff6512
MrTypeError <sanugta8@gmail.com>
2022-06-18 03:17:49 +0530

    Little Update

--- Recurssion/RecurssionEmoji.py
+++ Recurssion/RecurssionEmoji.py
@@ -1,7 +1,6 @@
-def design(num, spc=0):
+def design(num, spc=0,spc2=0):
 
     if num == 0:
-        print("Another One")
         return
         
 
@@ -9,8 +8,13 @@ def design(num, spc=0):
         spc += 2
         print(((" :) "*num) + " " * (spc*2-4)*2 + ":" + (str(num))))
         design(num-2, spc)
-
-
+    
+    if True:
+        def ReverseFun(spc2):
+            spc2 += 2
+            print(((" :) "*spc2) + " " * (num*2-4)*2 + ":" + (str(spc2))))
+            design(num-2, spc)
+        
 
 
 
