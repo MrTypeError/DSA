f19b5fa5d4ad2707d77690c1f3b9caa134571a4a
MrTypeError <sanugta8@gmail.com>
2022-06-26 02:30:24 +0530

    Addition of Sorting

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -1,2 +1,9 @@
+[10,20,3,5,6,77,3,73,7,7]
 def InSsorting(array,size):
-    for i in range 
+    for s in range (len(array)):
+        min=s
+        for i in range(s+1,size):
+            if array[i]<array[min]:
+                min=i
+            array[i]=array[]
+        
