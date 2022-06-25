a64e7fa9e1094824b484c068d74582e3d9cf518b
MrTypeError <sanugta8@gmail.com>
2022-06-26 02:59:06 +0530

    InsertionSort Updated

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -1,9 +1,15 @@
-[10,20,3,5,6,77,3,73,7,7]
-def InSsorting(array,size):
-    for s in range (len(array)):
+# sample array
+#[10,20,3,5,6,77,3,73,7,7]
+
+def InsertionSorting(array,size):
+    for s in range (size):
         min=s
         for i in range(s+1,size):
             if array[i]<array[min]:
                 min=i
-            array[i]=array[]
-        
+        array[s],array[min]=array[min],array[s]
+    print(array)
+
+# calling the function
+arr=[10,20,3,5,6,77,3,73,7,7]
+InsertionSorting(arr,10)
