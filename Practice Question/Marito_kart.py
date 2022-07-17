bc1bdb5ca24e116b12963a2ee488990f2edab53c
MrTypeError <sanugta8@gmail.com>
2022-07-17 23:17:53 +0530

    Coding In Progress (Marito Kart)

---
+++ Practice Question/Marito_kart.py
@@ -0,0 +1,43 @@
+'''Input:
+    7 2
+    1 41
+    3 47
+    6 41
+    4 50
+    7 36
+    2 47
+    5 50
+Output:
+    1 6
+Explanation:
+    7 2 is the size of the leaderboard. The second lowest score is 41 and the corresponding players are 1 and 6.
+
+####################################################################################################################################################################################
+
+Input:
+    5 2
+    3 37
+    1 41
+    2 37
+    5 41
+    4 35
+Output:
+    2 3
+Explanation:
+     5 2 is the size of the leaderboard. The second lowest score is 37 and their corresponding players are 2 and 3.
+'''
+mat=[[5,2],
+    [3,37],
+    [1,41],
+    [2,37],
+    [5,41],
+    [4,35]]
+m={}
+for key, value in m:
+    m[key]=value
+
+for i in len(mat):
+    for j in len(mat):
+        
+
+
