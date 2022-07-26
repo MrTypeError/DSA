88c4976b7e9948b749840f20fb2ea68b50fbb2f7
MrTypeError <sanugta8@gmail.com>
2022-07-26 20:18:08 +0530

    Level Order Traversal

---
+++ Trees/Traversal_In_Trees/Level_Order.py
@@ -0,0 +1,24 @@
+from collections import deque
+class Node:
+    def __init__(self , data , left , right) -> None:
+        self.data= data
+        self.left = left
+        self.right = right
+
+
+    def level_order(root):
+        # add element to start the queue and then start the loop
+        q=deque([root])
+        while q:
+            #pop One element and print the element and then add left or right which exists
+            temp = q.pop() 
+            print(temp.data)
+            #It will append the element in the queue (the left elements)
+            if temp.left:
+                q.appendleft(temp.left)
+            #It will append the element in the queue (the right elements) 
+            if temp.right:
+                q.appendleft(temp.right)
+
+
+a= Node("A", Node("B" , Node("D") , Node("E")), Node("C" , Node("F"),Node("G")))
