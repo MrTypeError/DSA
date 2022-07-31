b9fc815eb8ee8c6da239c628e12522b85da09fe4
MrTypeError <sanugta8@gmail.com>
2022-07-31 21:39:25 +0530

    Lowest Common Ancestor (LCA)

---
+++ Trees/Traversal_In_Trees/Lowest_common_Ancestor.py
@@ -0,0 +1,9 @@
+def solve(root, p, q):
+    if root:
+        if root.val == p or root.val == q:
+            return root
+        l, r = solve(root.left , p ,q) ,solve(root.right , p ,q)
+        if l and r :
+            return root 
+        return l if l else r
+    return None 
