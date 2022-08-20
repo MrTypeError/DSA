9c584ece3e65bbbd739bb260048da543229b2d2a
MrTypeError <sanugta8@gmail.com>
2022-08-20 20:02:18 +0530

    Name Correction

--- Graphs /DFS.py
+++ Graphs /DFS.py
@@ -1,6 +1,15 @@
 from collections import deque
 
 
+def adjecency_matrix_Undirected(edges):
+    NumOfNode = 1+ max([e[1]for e in edges] + [e[0]for e in edges] )
+    adj = [[0]*NumOfNode  for i in range(NumOfNode)]
+    for e in edges:
+        adj[e[0]][e[1]] = adj[e[1]][e[0]] = 1
+    return adj
+
+
+
 def DFS(adj):
     print("DFS")
     vis = set()
@@ -17,3 +26,14 @@ def DFS(adj):
                 if adj[el][adjel] and adjel not in vis :
                     vis.add(adjel)
                     st.append(adjel)
+
+
+Undirected_edges = adjecency_matrix_Undirected([[0,1],
+                                                [1,2],
+                                                [1,3],
+                                                [2,4],
+                                                [3,4],
+                                                [5,7],
+                                                [7,6]]) 
+print(Undirected_edges)
+
