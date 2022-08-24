37bd852f7352fdd983f10583a118dc0bb9274db1
MrTypeError <sanugta8@gmail.com>
2022-08-24 21:07:31 +0530

    Search Inseart Position (Bianary Search Problem )

---
+++ Searching/Bianary Search and Questions/Search_Inseart_position.py
@@ -0,0 +1,20 @@
+def solve(n, nums, target):
+    left = 0
+    right = len(nums) -1
+    
+    while left <= right:
+        mid = (right + left) // 2
+        
+        if nums[mid] == target:
+            return mid
+        
+        elif nums[mid]  < target:
+            left = mid+1
+        
+        elif nums[mid] > target:
+            right = mid-1
+        
+    return left
+
+
+print(solve(5,[1,3,4,5,10],2))
